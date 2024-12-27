0e5838ed09006569a69d434308986ca7f54001f3
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-27 12:31:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function majorityElement(arr) {
     let candidate = null;
     let count = 0;
 
-    // First pass: find the candidate element
     for (let num of arr) {
         if (count === 0) {
             candidate = num;
@@ -15,11 +14,8 @@ function majorityElement(arr) {
         }
     }
 
-    // Second pass: (Optional) Verify that candidate is the majority element
-    // We don't need this because the problem guarantees the majority element exists.
     return candidate;
 }
 
-// Test the function
 const input = [2, 1, 2];
-console.log(majorityElement(input)); // Output: 2
+console.log(majorityElement(input));
