c6298ccc64464c2236d13d44d3f3a169e437ce20
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-27 14:11:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ function majorityElement(arr) {
 }
 
 module.exports = majorityElement;
+
+
+
+/*
+function majorityElementChecker(arr)
+{
+	let map = new Map();
+
+	for(let i = 0; i < arr.length; i++) {
+		if (map.has(arr[i])) {
+				let count = map.get(arr[i]) +1;
+				if (count > arr.length /2) {
+					return arr[i];
+				} else
+					map.set(arr[i], count);
+
+		}
+		else
+			map.set(arr[i],1);
+		}
+		return 1;
+}
+*/
